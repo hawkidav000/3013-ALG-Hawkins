6e2f4ffd8997169852dc22dc807fc5b51f1a4eff
hawkidav000 <david.james@hawkinsonline.us>
2020-02-18 22:16:59 -0600

    fixed getching and deleted unnecessary files

--- Assignments/P02/main.cpp
+++ Assignments/P02/main.cpp
@@ -32,8 +32,7 @@ public:
     }
 
     int GetSize(){
-        //return size of the list
-        return 0;
+        return size;
     }
 
     void Insert(string w, string def){
@@ -49,9 +48,21 @@ public:
     }
 
     string Remove(){
-        //Im thinking Ill just remove from the front of the list
-        //Just to keep it easy
-        return " ";
+        if (head != NULL){
+            wordNode* temp = head;
+            string data;
+
+            data = temp->word;
+            head = head->next;
+            delete temp;
+            temp = NULL;
+            size--;
+
+            return data;
+        }
+        else{
+            return "Can't remove from empty list.";
+        }
     }
 
     string PrintList(){
@@ -74,30 +85,90 @@ public:
         return list;
     }
 
+    void Search(string item, int & match, string& results){
+        wordNode* temp = head;
+        string test;
+
+        while (temp != NULL){
+            test = temp->word;
+            if(item == test.substr(0, item.length())){
+                match++;
+                if (match <= 10){
+                    results += temp->word + ", ";
+                }
+
+            }
+            temp = temp->next;
+        }
+    }
+
 
 };
 
+/*****************************************************
+ * Function Prototypes
+ ****************************************************/
+string GetchInput();
+void LoadList(Timer& T, LinkedList& L, JsonFacade& J, double& sec, long& mill);
+void PrintResults(int& match, string& results, Timer& S);
+
 int main(){
 
-    Timer T;
-    LinkedList L;
+    Timer T;                            //Timer object for loading list
+    Timer S;                            //Timer object for searching
+    LinkedList L;                       //Linked list object
+    double Sec;                         //num of secs to load list
+    long Milli;                         //num of millisecs to load list
+    int match = 0;                      //used to record number of matches
+    string result;                      //records first ten results from search
+    string item;                        //holds what we're searching for
 
-    JsonFacade J("dict_w_defs.json");
+    JsonFacade J("dict_w_defs.json");   //Json object holding list of words
 
+    LoadList(T, L, J, Sec, Milli);      //Loads list & times it
+
+    item = GetchInput();
+
+    S.Start();
+    L.Search(item, match, result);
+    S.End();
+
+    PrintResults(match, result, S);
+ 
+    return 0;
+}
+
+string GetchInput(){
+    char k;             // holder for character being typed
+    string word = "";   // var to concatenate letters to
+
+    cout << "Type what you're searching for, then press enter: ";
+
+    while ((k = getch()) != 13) {
+        word += k;
+        cout << k;
+    }
+    
+    cout << endl;
+
+    return word;
+}
+
+void LoadList(Timer& T, LinkedList& L, JsonFacade& J, double& sec, long& mill){
     T.Start();
     for (int i = 0; i < J.getSize(); i++){
         L.Insert(J.getKey(i), J.getValue(J.getKey(i)));
     }
     T.End();
 
-    double s = T.Seconds();
-    long m = T.MilliSeconds();
+    sec = T.Seconds();
+    mill = T.MilliSeconds();
+}
 
-    cout << "Seconds: " << s << endl;
-    cout << "Milliseconds: " << m << endl;
 
-    string link = L.PrintTail();
-    cout << link;
 
-    return 0;
+void PrintResults(int& match, string& results, Timer& S){
+    cout << endl << match << " words found in " << S.Seconds() 
+        << " seconds" << endl << endl;
+    cout << results << endl;
 }
